46141c6c75244b1e5c52a268605c9f52cdd2fe74
anhmmo <aun@metropolia.fi>
2020-01-21 22:21:17 +0200

    yes

--- main.js
+++ main.js
@@ -1,32 +1,29 @@
 'use strict';
     
-    const KEY_NAME = "thiskey";
+    const KEY_NAME = "STUDENTS";
 
-    function getInputValue(elementti){
-        let inputValue = document.getElementById(elementti).value;
-        return inputValue;
-    }
+    const Student = {
+        ListStudent : [],
 
-    var listStudents = {
-        dataList : [],
         add: function(student){
-            this.dataList.push(student)
+            this.ListStudent.push(student)
         },
+
         save: function(){
-            const jsonData = JSON.stringify(this.dataList);
+            const jsonData = JSON.stringify(this.ListStudent);
             localStorage.setItem(KEY_NAME, jsonData);
         }
 
     };
 
     const formAuto = document.getElementById('main-form');
-    formAuto.addEventListener('submit', function(film3) {
-    film3.preventDefault();
-});
+        formAuto.addEventListener('submit', function(e) {
+            e.preventDefault();
+        });
 
     let buttonClicked = document.getElementById("submit");
-    buttonClicked.addEventListener("click", function(film){
-        film.preventDefault();
+    buttonClicked.addEventListener("click", function(){
+
         let name = getInputValue("name");
         let address = getInputValue("address");
         let phone = getInputValue("phone");
@@ -34,7 +31,7 @@
         let age = getInputValue("age");
         let selected = changeSelectedValue(getInputValue("selectOption"));
       
-        listStudents.add(
+        Student.add(
             {
                 name: name,
                 address: address,
@@ -45,12 +42,79 @@
             }
         );
 
-        
-        
-        listStudents.save();
+        Student.save();
+        renderToHtml();
     });
-    
 
+
+    
+    let outputJson = localStorage.getItem(KEY_NAME);
+    let dataOutput = JSON.parse(outputJson);
+    let itembox = document.getElementById("item__box");
+    console.log(dataOutput.length);
+
+    function renderToHtml(){
+        
+        for(let i = 0; i < dataOutput.length; i++){
+            
+            let divInfo = document.createElement("div");
+            divInfo.setAttribute("class", "item__box--info");
+            let divIcon = document.createElement("div");
+            divIcon.setAttribute("class", "item__box--icon");
+
+            let pName = document.createElement("p");
+            let pPhone = document.createElement("p");
+            let pAddress = document.createElement("p");
+            let pJob = document.createElement("p");
+            let pEmail = document.createElement("p");
+            let pAge = document.createElement("p");
+
+            let nameText = document.createTextNode("Name: Nguyen");
+            pName.appendChild(nameText);
+            let phoneText = document.createTextNode("Name: Nguyen");
+            pPhone.appendChild(phoneText);
+            let addressText = document.createTextNode("Name: Nguyen");
+            pAddress.appendChild(addressText);
+            let emailText = document.createTextNode("Name: Nguyen");
+            pEmail.appendChild(emailText);
+            let ageText = document.createTextNode("Name: Nguyen");
+            pAge.appendChild(ageText);
+            let jobText = document.createTextNode("Name: Nguyen");
+            pJob.appendChild(jobText);
+
+
+            divInfo.appendChild(pName);
+            divInfo.appendChild(pPhone);
+            divInfo.appendChild(pAddress);
+            divInfo.appendChild(pAge);
+            divInfo.appendChild(pJob);
+            divInfo.appendChild(pEmail);
+
+            let deleteIcon = document.createElement("i");
+            let editIcon = document.createElement("i");
+
+            let iconText1 = document.createTextNode('<i id="delete" class="fas fa-trash-alt"></i>');
+            let iconText2 = document.createTextNode('<i id="edit" class="fas fa-edit"></i>');
+
+            deleteIcon.appendChild(iconText1);
+            editIcon.appendChild(iconText2);
+
+            divIcon.appendChild(deleteIcon);
+            divIcon.appendChild(editIcon);
+
+            itembox.appendChild(divIcon);
+            itembox.appendChild(divInfo);
+
+        }
+        
+    }
+
+    
+    function getInputValue(elementti){
+        let inputValue = document.getElementById(elementti).value;
+        return inputValue;
+    }
+    
     function changeSelectedValue(getValue) {
         switch(getValue){
             case "1":
@@ -68,7 +132,5 @@
             case "4":
             return "Sofware engineer";
             break;
-
-            
         }
     }
