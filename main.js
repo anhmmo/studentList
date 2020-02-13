a559a6491115bc19d9ad7d9cbc79b36128a40e1b
anhmmo <aun@metropolia.fi>
2020-02-13 10:53:34 +0200

    call onscroll event when user scroll down more than 500px, icon show if scroll less than 500x icon display none.

--- main.js
+++ main.js
@@ -683,4 +683,6 @@ function changecolorToWhite() {
 let iconToTheTop = document.getElementById("toTheTop");
 iconToTheTop.style.display = "none";
 
+
+//call onscroll event when user scroll down more than 500px, icon show if scroll less than 500x icon display none.
 listItem.onscroll = () => listItem.scrollTop > 500 ? iconToTheTop.style.display = "block" : iconToTheTop.style.display = "none";
